ebf67c42031522759357c1d8b545952dea5e75a8
Julien Schneider <julien.schneider.1991@gmail.com>
2019-12-05 21:17:23 +0100

    refactor: components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,42 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
+import {AppComponent} from './components/app/app.component';
 import {Route, RouterModule} from '@angular/router';
 import {ClarityModule} from '@clr/angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { VisionComponent } from './components/vision/vision.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 export const routes: Route[] = [{
   path: 'mes-bons-plans',
-  loadChildren: () => import('./../main-tab/main-tab.module').then(m => m.MainTabModule)
+  loadChildren: () => import('./components/main-tab/main-tab.module').then(m => m.MainTabModule)
 },
   {
     path: 'mon-profil',
-    loadChildren: () => import('./../profile-tab/profile-tab.module').then(m => m.ProfileTabModule)
+    loadChildren: () => import('./components/profile-tab/profile-tab.module').then(m => m.ProfileTabModule)
   },
   {
     path: 'register',
     loadChildren: () => import('./../register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'vision', component: VisionComponent
   }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
