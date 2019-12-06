bbbc4699846eb1bd02127d59d74d566617e2cecd
Julien Schneider <julien.schneider.1991@gmail.com>
2019-12-06 05:09:07 +0100

    feat: improve style

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -7,4 +7,31 @@ import {Component} from '@angular/core';
 })
 export class FilterComponent {
   filter = {city: '', category: 'santé'};
+
+  categories = [
+    {name: 'Santé', icon: 'heart'},
+    {name: 'Logement', icon: 'home'},
+    {name: 'Transport', icon: 'car'},
+    {name: 'Alimentation', icon: 'fish'},
+    {name: 'Sorties', icon: 'happy-face'},
+    {name: 'Scolaires', icon: 'flask'},
+  ];
+
+  cities = [
+    {name: 'Grenoble', icon: 'map-marker'},
+    {name: 'Paris', icon: 'map-marker'},
+  ];
+
+  showMenuCategory = false;
+  showMenuCities = false;
+
+  toggleCategory() {
+    this.showMenuCategory = !this.showMenuCategory;
+    this.showMenuCities = false;
+  }
+
+  toggleCities() {
+    this.showMenuCities = !this.showMenuCities;
+    this.showMenuCategory = false;
+  }
 }
